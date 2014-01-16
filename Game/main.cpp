d7b340f29e97c20f3e03b232f42fcf556d85f395
mikael <mikael@Mikaels-MacBook-Pro.local>
2014-01-16 12:31:26 +0100

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by mikael on 16/01/14.
+//  Copyright (c) 2014 mikael. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
