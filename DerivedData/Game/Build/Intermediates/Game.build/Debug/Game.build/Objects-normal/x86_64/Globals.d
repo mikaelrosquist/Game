0a9bcb42c36bf1d3c79850cabc6cdc79045e82a9
Mikael Rosquist <mikael@rosquist.se>
2014-07-15 15:00:40 +0200

    Initial upload

---
+++ DerivedData/Game/Build/Intermediates/Game.build/Debug/Game.build/Objects-normal/x86_64/Globals.d
@@ -0,0 +1,44 @@
+dependencies: \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Globals.cpp \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Globals.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_main.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_stdinc.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_config.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_platform.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_config_macosx.h \
+  /Library/Frameworks/SDL.framework/Headers/begin_code.h \
+  /Library/Frameworks/SDL.framework/Headers/close_code.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_audio.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_error.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_endian.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_mutex.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_thread.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_rwops.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_cdrom.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_cpuinfo.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_events.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_active.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_keyboard.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_keysym.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_mouse.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_video.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_joystick.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_quit.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_loadso.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_timer.h \
+  /Library/Frameworks/SDL.framework/Headers/SDL_version.h \
+  /Library/Frameworks/SDL_ttf.framework/Headers/SDL_ttf.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Frame.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Sprite.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Image.h \
+  /Library/Frameworks/SDL_image.framework/Headers/SDL_image.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Rectangle.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/MinBug.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Bug.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/MedBug.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/BigBug.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Ship.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Player.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Shield.h \
+  /Users/mikaelrosquist/Documents/Mikael/Plugg/Year\ III/Prog3/SpaceInvaders/Game/Text.h
